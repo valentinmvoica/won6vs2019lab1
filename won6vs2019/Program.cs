04cfd9d191c36c90df71b20912c5ca572c03cb35
Valentin Voica <valentin_voiclla@live.com>
2023-06-29 17:28:10 +0300

    Add project files.

---
+++ won6vs2019/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace won6vs2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+              paralelipiped dreptunghic, va afisa volumul lui
+             */
+
+            Console.WriteLine("introduceti latime");
+            int latime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("introduceti latime");
+            int lungime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("introduceti latime");
+            int inaltime = int.Parse(Console.ReadLine());
+
+            int volumulParalelipipedului = latime * inaltime * lungime;
+
+            Console.WriteLine(volumulParalelipipedului);
+
+        }
+    }
+}
